66c505dba71cb6636e76c088a21a57915f09c41b
Spenser Black <spenserblack01@gmail.com>
2020-01-24 21:38:45 -0500

    Fix reversible macro

--- src/lib.rs
+++ src/lib.rs
@@ -225,7 +225,7 @@ macro_rules! versions {
         $crate::get_versions($crate_name, $user_agent)
     };
     (user_agent = $user_agent:expr, crate_name = $crate_name:expr $(,)?) => {
-        $crate::versions!($crate_name, $user_agent)
+        $crate::get_versions($crate_name, $user_agent)
     };
     (crate_name = $crate_name:expr $(,)?) => {
         $crate::versions!(crate_name = $crate_name, user_agent = $crate::user_agent!())
