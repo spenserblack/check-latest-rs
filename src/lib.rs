3826b50d31bfaa45e30073322e0639fd77edc7a9
Spenser Black <spenserblack01@gmail.com>
2020-01-24 21:39:12 -0500

    Add max_version!

--- src/lib.rs
+++ src/lib.rs
@@ -86,6 +86,7 @@ pub fn get_versions(crate_name: &str, user_agent: &str) -> Result<Versions, Erro
     Ok(versions)
 }
 
+/// *__NOTE__ You probably want to use `max_version!`*
 /// Compares the current crate version to the maximum version available on
 /// [Crates.io].
 ///
@@ -232,5 +233,147 @@ macro_rules! versions {
     };
     (user_agent = $user_agent:expr $(,)?) => {
         $crate::versions!(crate_name = $crate::crate_name!(), user_agent = $user_agent)
+    };
+}
+///
+/// Makes it easier to run `get_max_version`.
+///
+/// `max_version!()` will predict the `crate_name`, `current_crate_version`, and
+/// `user_agent`.
+/// `crate_name` will default to package name in your `Cargo.toml` file.
+/// `current_crate_version` will default to the package version in your
+/// `Cargo.toml` file.
+/// `user_agent` will default to the name of your crate as defined in
+/// `Cargo.toml` followed by a `/` and the version of your package as defined
+/// in your `Cargo.toml` file (e.g. `my-crate-name/1.0.0`).
+///
+/// If you do not want these defaults to be used, you can set your own values.
+/// See the examples below.
+///
+/// # Examples
+///
+/// ## Use Defaults
+///
+/// ```rust,no_run
+/// use check_latest::max_version;
+///
+/// if let Ok(Some(version)) = max_version!() {
+///     println!("Go get version {}!", version);
+/// }
+/// ```
+///
+/// ## Set Crate Name
+///
+/// ```rust,no_run
+/// use check_latest::max_version;
+///
+///     let name = "my-renamed-crate";
+///
+/// if let Ok(Some(version)) = max_version!(crate_name = name) {
+///     println!("Go get version {}!", version);
+/// }
+/// ```
+///
+/// ## Set Crate Version to Compare
+///
+/// ```rust,no_run
+/// use check_latest::max_version;
+///
+/// let current_version = "1.2.3";
+///
+///
+/// if let Ok(Some(version)) = max_version!(version = current_version) {
+///     println!("Go get version {}!", version);
+/// }
+/// ```
+///
+/// ## Set User Agent
+///
+/// ```rust,no_run
+/// use check_latest::max_version;
+///
+/// let user_agent = "My extra detailed user agent";
+///
+/// if let Ok(Some(version)) = max_version!(user_agent = user_agent) {
+///     println!("Go get version {}!", version);
+/// }
+/// ```
+///
+/// ## Set All 3
+///
+/// ```rust,no_run
+/// use check_latest::max_version;
+///
+/// let crate_name = "my-renamed-crate";
+/// let current_version = "1.2.3";
+/// let user_agent = "My extra detailed user agent";
+///
+/// let max_version = max_version!(
+///     // These can be shuffled BTW
+///     crate_name = crate_name,
+///     version = current_version,
+///     user_agent = user_agent,
+/// );
+///
+/// if let Ok(Some(version)) = max_version {
+///     println!("Go get version {}!", version);
+/// }
+/// ```
+#[macro_export]
+macro_rules! max_version {
+    () => {
+        $crate::max_version!(crate_name = $crate::crate_name!(), version = $crate::crate_version!(), user_agent = $crate::user_agent!())
+    };
+    // All 3 specified {{{
+    (crate_name = $crate_name:expr, version = $version:expr, user_agent = $user_agent:expr $(,)?) => {
+        $crate::get_max_version($crate_name, $version, $user_agent)
+    };
+    (crate_name = $crate_name:expr, user_agent = $user_agent:expr, version = $version:expr $(,)?) => {
+        $crate::get_max_version($crate_name, $version, $user_agent)
+    };
+    (version = $version:expr, crate_name = $crate_name:expr, user_agent = $user_agent:expr $(,)?) => {
+        $crate::get_max_version($crate_name, $version, $user_agent)
+    };
+    (version = $version:expr, user_agent = $user_agent:expr, crate_name = $crate_name:expr $(,)?) => {
+        $crate::get_max_version($crate_name, $version, $user_agent)
+    };
+    (user_agent = $user_agent:expr, crate_name = $crate_name:expr, version = $version:expr $(,)?) => {
+        $crate::get_max_version($crate_name, $version, $user_agent)
+    };
+    (user_agent = $user_agent:expr, version = $version:expr, crate_name = $crate_name:expr $(,)?) => {
+        $crate::get_max_version($crate_name, $version, $user_agent)
+    };
+
+    (version = $version:expr, user_agent = $user_agent:expr $(,)?) => {
+        $crate::get_max_version($crate::crate_name!(), $version, $user_agent)
+    };
+    (user_agent = $user_agent:expr, version = $version:expr $(,)?) => {
+        $crate::get_max_version($crate::crate_name!(), $version, $user_agent)
+    };
+    (crate_name = $crate_name:expr, user_agent = $user_agent:expr $(,)?) => {
+        $crate::get_max_version($crate_name, $crate::crate_version!(), $user_agent)
+    };
+    (user_agent = $user_agent:expr, crate_name = $crate_name:expr $(,)?) => {
+        $crate::get_max_version($crate_name, $crate::crate_version!(), $user_agent)
+    };
+    (crate_name = $crate_name:expr, version = $version:expr $(,)?) => {
+        $crate::get_max_version($crate_name, $version, $crate::user_agent!())
+    };
+    (version = $version:expr, crate_name = $crate_name:expr $(,)?) => {
+        $crate::get_max_version($crate_name, $version, $crate::user_agent!())
+    };
+
+    (crate_name = $crate_name:expr $(,)?) => {
+        $crate::get_max_version($crate_name, $crate::crate_version!(), $crate::user_agent!())
+    };
+    (version = $version:expr $(,)?) => {
+        $crate::get_max_version($crate::crate_name!(), $version, $crate::user_agent!())
+    };
+    (user_agent = $user_agent:expr $(,)?) => {
+        $crate::get_max_version($crate::crate_name!(), $crate::crate_version!(), $user_agent)
+    };
+
+    () => {
+        $crate::get_max_version($crate::crate_name!(), $crate::crate_version!(), $crate::user_agent!())
     };
 }
