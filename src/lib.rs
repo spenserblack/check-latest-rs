222a7b5c629e33bcfe376b655c03fc486e63f06b
Spenser Black <spenserblack01@gmail.com>
2020-01-31 08:03:57 -0500

    Add `fn`s for getting max version

--- src/lib.rs
+++ src/lib.rs
@@ -42,6 +42,56 @@ pub struct Version {
 }
 
 impl Versions {
+    /// Gets *any* max version.
+    ///
+    /// # Example
+    ///
+    /// ```rust,no_run
+    /// use check_latest::Versions;
+    ///
+    /// let newest = Versions::new("my-cool-crate", "my-cool-crate/1.0.0")
+    ///     .unwrap()
+    ///     .max_version()
+    /// ```
+    pub fn max_version(&self) -> Option<&Version> {
+        self.versions
+            .iter()
+            .max_by(|v1, v2| v1.version.cmp(&v2.version))
+    }
+    /// Gets the max version that hasn't been yanked.
+    ///
+    /// # Example
+    ///
+    /// ```rust,no_run
+    /// use check_latest::Versions;
+    ///
+    /// let newest = Versions::new("my-cool-crate", "my-cool-crate/1.0.0")
+    ///     .unwrap()
+    ///     .max_unyanked_version()
+    /// ```
+    pub fn max_unyanked_version(&self) -> Option<&Version> {
+        self.versions
+            .iter()
+            .filter(|v| !v.yanked)
+            .max_by(|v1, v2| v1.version.cmp(&v2.version))
+    }
+    /// Gets max version that has been yanked.
+    ///
+    /// # Example
+    ///
+    /// ```rust,no_run
+    /// use check_latest::Versions;
+    ///
+    /// let newest = Versions::new("my-cool-crate", "my-cool-crate/1.0.0")
+    ///     .unwrap()
+    ///     .newest_yanked_version()
+    /// ```
+    pub fn max_yanked_version(&self) -> Option<&Version> {
+        self.versions
+            .iter()
+            .filter(|v| v.yanked)
+            .max_by(|v1, v2| v1.version.cmp(&v2.version))
+    }
     /// Gets *any* newest version.
     ///
     /// # Example
