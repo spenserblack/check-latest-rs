4918268069d9ce30c73b88749f4518b33be9b8d7
Spenser Black <spenserblack01@gmail.com>
2020-02-03 13:16:14 -0500

    Deprecate macros

--- src/blocking/max.rs
+++ src/blocking/max.rs
@@ -412,6 +412,7 @@ macro_rules! max_version {
 /// }
 /// ```
 #[macro_export]
+#[deprecated(since = "1"]
 macro_rules! max_minor_version {
     () => {
         $crate::max_minor_version!(
@@ -610,6 +611,7 @@ macro_rules! max_minor_version {
 /// }
 /// ```
 #[macro_export]
+#[deprecated(since = "1"]
 macro_rules! max_patch {
     () => {
         $crate::max_patch!(
