9431c59804393433f7f923e97d8965ce6a74c07e
Spenser Black <spenserblack01@gmail.com>
2020-02-04 08:54:24 -0500

    Remove deprecated usage from basic example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,11 +1,12 @@
-use check_latest::max_version;
-
 fn main() {
     // We're setting the version to check against so that we can safely assume
     // that this example will *always* find a higher version.
     let version = "0.0.1";
 
-    if let Ok(Some(version)) = max_version!(version = version) {
+    if let Ok(Some(version)) = check_latest::check_max!(version) {
         println!("A new version has been released: {}", version);
+    }
+    if let Ok(Some(version)) = check_latest::check_minor!(version) {
+        println!("A new minor version has been released: {}", version);
     }
 }
